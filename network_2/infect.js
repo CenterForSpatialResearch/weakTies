14e1d65787ad4113c2cfe429f4a747a0b0db1063
jz3077 <jz3077@columbia.edu>
2020-04-10 22:24:40 -0400

    add step label

--- network_2/infect.js
+++ network_2/infect.js
@@ -2,7 +2,7 @@ var delayInterval = 10
 var colorIndex = 1
 var oldFriends = []
 var cScale = d3.scaleLinear().domain([1,6,12]).range(["magenta","gold","cyan"])
-
+var sum = 0
 d3.selectAll(".networkCircle")
 .attr("fill","#ffffff")
 .attr("cursor","pointer")
@@ -13,30 +13,47 @@ d3.selectAll(".networkCircle")
 
     var id = d3.select(this).attr("class").split(" ")[0]
     //node_38_node_65
+    d3.selectAll(".step").remove()
     
     oldFriends = [id]
     var friends = getFriends(id)
     changeFriends(friends)
-    d3.select(this).attr("fill","black")
+    
+    d3.select(this).attr("fill","magenta")
+            var x = parseFloat(d3.select("."+id).attr("cx"))
+            var y = parseFloat(d3.select("."+id).attr("cy"))
+    console.log([x,y])
+    d3.select("#chart1 svg").append("text").text("0").attr("x",x+nodeRadius).attr("y",y).attr("class","step")
+    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
    
 })
 function changeFriends(friends){
-    
-    colorIndex+=1
     d3.select("#steps").html("It takes <span id = \"highlight\">"+colorIndex+"</span> steps to reach everyone")
     
     var newFriends = []
     for(var f in friends){
         if(oldFriends.indexOf(friends[f])==-1){
             oldFriends.push(friends[f])
+            console.log(friends[f])
+            var x = d3.select("."+friends[f]).attr("cx")
+            var y = d3.select("."+friends[f]).attr("cy")
+            console.log([x,y])
+        d3.select("#chart1 svg").append("text").text(colorIndex).attr("x",x+nodeRadius).attr("y",y-nodeRadius).attr("class","step")
+        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
+            
             d3.select("."+friends[f]).transition().delay(colorIndex*100).attr("fill",cScale(colorIndex))
             var ff = getFriends(friends[f])
             newFriends = newFriends.concat(ff)
         }
     }
+    colorIndex+=1
+    
     newFriends = removeDups(newFriends)
+    sum+=newFriends.length
     
     oldFriends = removeDups(oldFriends)
+   // console.log([newFriends.length,oldFriends.length])
+
     if(oldFriends.length==nodes.length){
         return
     }
