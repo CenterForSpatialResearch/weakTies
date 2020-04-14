cb73e47a981d7641d6178bfd59b021b4c526b1a6
jz3077 <jz3077@columbia.edu>
2020-04-13 21:47:13 -0400

    static

---
+++ network_2/infect_static.js
@@ -0,0 +1,146 @@
+var delayInterval = 10
+var colorIndex = 1
+var oldFriends = []
+var cScale = d3.scaleLinear().domain([1,6,12]).range(["magenta","gold","cyan"])
+var distances = []
+
+d3.selectAll(".networkCircle")
+//.attr("fill","#ffffff")
+.attr("cursor","pointer")
+.on('click',function(){
+    distances = []
+    colorIndex = 1
+   // d3.selectAll(".networkCircle").attr('fill',"#ffffff")
+    d3.selectAll(".link").attr('stroke',"#000000").attr("opacity",.2)
+
+    var id = d3.select(this).attr("class").split(" ")[0]
+    //node_38_node_65
+    d3.selectAll(".step").remove()
+    
+    oldFriends = [id]
+    var friends = getFriends(id)
+	console.log(id)
+	console.log(links)
+	console.log(friends)
+	
+    changeFriends(friends)
+    var x = parseFloat(d3.select("."+id).attr("cx"))
+    var y = parseFloat(d3.select("."+id).attr("cy"))
+    d3.select("#chart1 svg").append("text").text("0").attr("x",x+nodeRadius).attr("y",y-nodeRadius).attr("class","step")
+    //.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
+    d3.select(this).attr("fill","magenta")
+   distanceHistogram(id)
+})
+
+function getDistances(friends){
+    
+}
+
+function distanceHistogram(id){
+    var x = parseFloat(d3.select("."+id).attr("cx"))
+    var y = parseFloat(d3.select("."+id).attr("cy"))
+    var barWidth = 15
+    var height = 700
+    var width  = 500
+    d3.select("#chart2 svg").remove()
+    var svg = d3.select("#chart2").append("svg").attr("width",width).attr("height",height).append("g")
+    svg.selectAll("rect")
+    .data(distances)
+    .enter()
+    .append("rect")
+    .attr("x",function(d,i){return i*barWidth+barWidth})
+    .attr("y",function(d,i){return height-d.new*barWidth-barWidth*2})
+    .attr("width",barWidth-1)
+    .attr("height",function(d,i){return d.new*barWidth})
+    .attr("fill",function(d){return cScale(d.step)})
+    
+    svg.selectAll(".histoText")
+    .data(distances)
+    .enter()
+    .append("text")
+    .attr("x",function(d,i){return i*barWidth+barWidth*1.5})
+    .attr("y",function(d,i){return height-d.new*barWidth-barWidth*2})
+    .text(function(d,i){return d.new})
+    .attr("text-anchor","middle")
+    
+    svg.selectAll(".histoText")
+    .data(distances)
+    .enter()
+    .append("text")
+    .attr("x",function(d,i){return i*barWidth+barWidth*1.5})
+    .attr("y",function(d,i){return height-barWidth})
+    .text(function(d,i){return d.step})
+    .attr("text-anchor","middle")
+    
+    svg.append("text").text("number of steps").attr("x",barWidth).attr("y",height)
+    svg.append("text").text("number of nodes reached at step").attr("x",0).attr("y",height/2).style("writing-mode","vertical-rl")
+    
+    svg.attr("transform","translate(40,-40)")
+}
+function changeFriends(friends){
+    d3.select("#steps").html("It takes <span id = \"highlight\">"+colorIndex+"</span> steps to reach everyone")
+    
+    var newFriends = []
+    for(var f in friends){
+        if(oldFriends.indexOf(friends[f])==-1){
+            oldFriends.push(friends[f])
+            var x = d3.select("."+friends[f]).attr("cx")
+            var y = d3.select("."+friends[f]).attr("cy")
+            
+        d3.select("#chart1 svg").append("text").text(colorIndex).attr("x",x+nodeRadius).attr("y",y-nodeRadius).attr("class","step")
+       // .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
+            .attr("opacity",0)
+            .transition()
+            .delay(colorIndex*100)
+            .attr("opacity",1)
+            
+            d3.select("."+friends[f]).transition().delay(colorIndex*100).attr("fill",cScale(colorIndex))
+            var ff = getFriends(friends[f])
+            newFriends = newFriends.concat(ff)
+        }
+    }
+    newFriends = removeDups(newFriends)    
+    oldFriends = removeDups(oldFriends)
+    
+    if(colorIndex==1){
+        distances.push({step:colorIndex,new:oldFriends.length-1,old:oldFriends.length})
+    }else{
+        var lastCount = distances[distances.length-1].old
+        distances.push({step:colorIndex,new:oldFriends.length-lastCount,old:oldFriends.length})
+    }
+    
+    colorIndex+=1    
+
+    if(oldFriends.length==nodes.length){
+        return
+    }
+    changeFriends(newFriends)
+
+    //track
+    //get new friends
+    //stop only when friends.length is == nodes.length
+}
+
+function removeDups(friends){
+    var noDup = []
+    for(var i in friends){
+        if(noDup.indexOf(friends[i])==-1){
+            noDup.push(friends[i])
+        }
+    }
+    return noDup
+}
+
+function getFriends(id){
+    var friends = []
+    for(var l in links){
+        var link = links[l]
+        if(id ==link.source.id){
+            friends.push(link.target.id)
+        }else if(id == link.target.id){
+            friends.push(link.source.id)
+        }
+    }
+    return friends
+}
+
---
+++ network_2/run_staticLayout.js
@@ -0,0 +1,88 @@
+var className = "A"
+
+
+
+//var simulation = d3.forceSimulation(nodes)
+//            .force("charge", d3.forceManyBody().strength(strength))
+//            .force("link", d3.forceLink(links).distance(distance).strength(0.6).iterations(16))
+//            .force("x", d3.forceX())
+//            .force("y", d3.forceY())
+//.on("tick", ticked);
+
+    
+var g = svg.append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
+var link = g.append("g").attr("stroke", strokeColor).attr("stroke-width", strokeWidth).attr("class","link"+className)
+.selectAll(".link"+className)
+var node = g.append("g")
+    .attr("stroke", strokeColor).attr("stroke-width", strokeWidth).attr("stroke-opacity",.2)
+    .attr("class","node node"+className).selectAll(".node"+className);
+
+ //   restart();
+        
+/*
+    window.addEventListener("resize", function(){
+        var newWidth = window.innerWidth
+        strength = strengthScale(newWidth)
+        restart()
+    });*/
+
+
+    var sources
+    var sourcesMap
+    var targets
+    var targetsMap
+   
+    function restart() {
+      // Apply the general update pattern to the nodes.
+        svg.attr("width",window.innerWidth)
+        g.attr("transform", "translate(" +window.innerWidth/ 2 + "," + (window.innerHeight / 2) + ")")
+      node = node.data(nodes, function(d) { return d.id;});
+      node.exit().remove();
+      node = node.enter()
+          .append("circle")
+          .attr("class",function(d){return d.id+" networkCircle"})
+          .attr("r", nodeRadius)
+          .merge(node)
+      .on("mouseover",function(d){
+       //   console.log(d)
+      })
+          //.on("mouseover",function(d){
+          //    d3.select(this).attr("fill",function(d){
+          //        return groupColor[d.group]
+          //    })
+          //})
+        //added below to update color
+
+
+      // Apply the general update pattern to the links.
+      link = link.data(links, function(d) { return d.source.id + "-" + d.target.id; });
+      link.exit().remove();
+      link = link.enter()
+      .append("line")
+      .attr("class",function(d){
+         // if(parseInt(d.source.id)<parseInt(d.target.id)){
+             return "link "+d.source.id + "_" + d.target.id;
+          
+      })
+      .attr("stroke", strokeColor).attr("stroke-width",strokeWidth)
+      .attr("opacity",.2)
+      .merge(link);
+    //  // Update and restart the simulation.
+      
+//var simulation = d3.forceSimulation()
+//   .force("link",d3.forceLink().distance(function (d) {return d.l; }) .strength(0.03) .iterations(16))
+//.force("collide",d3.forceCollide().radius(function (d) {return d.r;}) .strength(0.7).iterations(16))
+//   .force("charge", d3.forceManyBody().strength(-200))
+//   .force("x", d3.forceX().strength(0.02).x(width / 2))
+//   .force("y", d3.forceY().strength(0.02).y(height / 2))
+      
+     // simulation.nodes(nodes);
+     // simulation.force("link").links(links) 
+     // simulation.force("charge", d3.forceManyBody().strength(-800))
+     //           .force("link", d3.forceLink(links).distance(distance).strength(0.1))
+     // .force("collide",d3.forceCollide().radius(50) .strength(0.1).iterations(16))
+     //
+     // simulation.force("x", d3.forceX())
+     //           .force("y", d3.forceY())
+     // simulation.alpha(1).restart();
+    }
